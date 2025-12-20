147ecc4fc60fc5c0cebb6c8458e70c31ef0dcf75
edris zeynu <endriszeynu173@gmail.com>
2025-12-20 09:07:55 +0300

    create user model

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app=express();
+dotenv.config();
+const app = express();
 
-app.get('/',(req,res)=>{
- res.send("hello world")
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('mongodb is connected');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000')
-})
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000');
+});
